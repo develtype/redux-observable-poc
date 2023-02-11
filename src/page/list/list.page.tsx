5745f85f973078436e8fe036ad2a997a009b4eef
develtype <develtype@gmail.com>
2023-02-11 21:09:17 +0900

    refactor: navigate and list page

--- src/page/list/list.page.tsx
+++ src/page/list/list.page.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { dataSelector } from 'src/redux/data/data.state';
+import styles from './list.scss';
 
 export const ListPage = () => {
+  const navigate = useNavigate();
   const datas = useSelector(dataSelector.datas);
+
+  function handleRowClick(id: string) {
+    navigate(id);
+  }
 
   return (
     <div>
       <h2>Data List</h2>
-      <ul>
-        {datas.map((data) => (
-          <li key={data.id}>{data.data}</li>
-        ))}
-      </ul>
+      <div className={styles.list}>
+        <div className={styles.head}>
+          <div className={styles.hCell}>Id</div>
+          <div className={styles.hCell}>Data</div>
+        </div>
+        <ul className={styles.body}>
+          {datas.map((data) => (
+            <li
+              key={data.id}
+              className={styles.bodyRow}
+              onClick={() => handleRowClick(data.id)}
+            >
+              <div className={styles.bCell}>{data.id}</div>
+              <div className={styles.bCell}>{data.data}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
