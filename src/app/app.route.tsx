e06ad8c77a86a8af7f6ed96ef427b39d4d4bc3e0
develtype <develtype@gmail.com>
2023-02-11 23:51:38 +0900

    refactor: detail and edit

--- src/app/app.route.tsx
+++ src/app/app.route.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { ItemPage } from 'src/page/detail/detail.page';
+import { DetailContainer } from 'src/page/detail/detail.container';
 import { ListPage } from 'src/page/list/list.page';
 
 export const AppRoute = () => {
   return (
     <Routes>
       <Route index element={<ListPage />} />
-      <Route path=':dataId' element={<ItemPage />} />
+      <Route path=':dataId' element={<DetailContainer />} />
       <Route path='*' element={<Navigate replace to='/' />} />
     </Routes>
   );
--- src/page/detail/detail.page.tsx
+++ src/page/detail/detail.page.tsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'src/component/atom/button/button';
+import { Input } from 'src/component/atom/input/input';
+import { dataAction } from 'src/redux/data/data.action';
+import { dataSelector } from 'src/redux/data/data.state';
+import { ReduxStatusEnum } from 'src/type/common.type';
+import { DataType } from 'src/type/data.type';
+import styles from './detail.scss';
 
-export const ItemPage = () => {
+type PropsType = {
+  data: DataType;
+};
+
+export const DetailPage = ({ data }: PropsType) => {
+  const dispatch = useDispatch();
+  const [editMode, setEditMode] = useState(false);
+  const [_data, set_data] = useState(data.data);
+  const [editClick, setEditClick] = useState(false);
+
+  const dataStatus = useSelector(dataSelector.status);
+
+  useEffect(() => {
+    if (editClick && dataStatus === ReduxStatusEnum.IDLE) {
+      setEditMode(false);
+      setEditClick(false);
+    }
+  }, [dataStatus]);
+
+  function handleEditBtnClick() {
+    setEditMode(!editMode);
+  }
+
+  function handleUpdateBtnClick() {
+    dispatch(dataAction.updateData.request({ id: data.id, data: _data }));
+    setEditClick(true);
+  }
+
   return (
-    <div>
-      <h2>Item Page</h2>
+    <div className={styles.root}>
+      <h2>{`${data.id}'s Detail`}</h2>
+      <div className={styles.control}>
+        <Button sizeType='sm' outlined={editMode} onClick={handleEditBtnClick}>
+          {editMode ? 'CANCEL' : 'EDIT'}
+        </Button>
+        {editMode && (
+          <Button
+            sizeType='sm'
+            onClick={handleUpdateBtnClick}
+            disabled={editClick}
+          >
+            {'UPDATE'}
+          </Button>
+        )}
+      </div>
+      <div className={styles.info}>
+        <div className={styles.name}>ID:</div>
+        <div className={styles.item}>{data.id}</div>
+        <div className={styles.name}>Data:</div>
+        <div className={styles.item}>
+          {editMode ? (
+            <Input
+              sizeType='md'
+              defaultValue={data.data}
+              onChange={(e) => set_data(e.currentTarget.value)}
+            />
+          ) : (
+            <>{data.data}</>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
